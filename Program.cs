6cfdea22afbead709133978c7b6fc2f382e90f5a
Dietrich Kirsch <eistitan@yahoo.com>
2022-11-14 11:15:08 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace ZooZwei
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SW1();
+        }
+
+        
+        static int Eingabe() //Eingabe der Zahl
+        {
+            int wahl = 0;
+            bool check;
+            do
+            {
+                Console.WriteLine("Wählen sie ein Tier:");
+                Console.WriteLine(" 1.Pferd\n 2.Tiger\n 3.Schildkröte\n 4.Storch\n 5.Gans");
+                check = int.TryParse(Console.ReadLine(), out wahl);
+                if (!check)
+                    Console.WriteLine("Nur die Ziffern eingeben.\n");
+                if ((check && wahl < 1) || (check && wahl > 5))
+                {
+                    Console.WriteLine("Sie haben sich verklickt.\n");
+                    check = false;
+                }
+            } while (!check);
+            return wahl;
+        }
+        public static string Menu2()
+        {
+            Console.WriteLine("Eine gute Wahl.");
+            Console.WriteLine("Möchten sie die Beschreibung(A) oder den Weg(B) zum Gehege sehen?");
+            string menu = Console.ReadLine().ToLower();
+            return menu;
+        }
+
+        public static void SW1()
+        {   int wahl =Eingabe();
+            string menu="";
+            switch (wahl)
+            {
+                case 1:
+                    mammal pferd = new("Hans", "Horsus schnellus", 4, false, true, "rot", false, "auf der Weide", "B-7");
+
+                    menu = Menu2();
+                    if (menu=="a")
+                    {
+                        pferd.Beschreibung();
+                    }else if (menu=="b")
+                        {
+                        pferd.Wegbeschreibung(); 
+                        }
+                    break;
+
+                case 2:
+                    mammal tiger = new("Rappu", "Tigrus altus", 4, false, true, "gestreift", true, "im Tiger-Dome", "K-2");
+
+                    menu = Menu2();
+                    if (menu == "a")
+                    {
+                        tiger.Beschreibung();
+                    }
+                    else if (menu == "b")
+                    {
+                        tiger.Wegbeschreibung();
+                    }
+                    break;
+                case 3:
+                    reptile schildkroete = new("Leonardo", "Ninja turtle", 4, false, false, "grün", false, "in der Kanalisation", "S-1");
+
+                    menu = Menu2();
+                    if (menu == "a")
+                    {
+                        schildkroete.Beschreibung();
+                    }
+                    else if (menu == "b")
+                    {
+                        schildkroete.Wegbeschreibung();
+                    }
+                    break;
+                case 4:
+                    vogel storch = new("Bomber", "Langbeinus entus", 2, true, false, "grau", false, "auf dem Chimney", "U-8");
+
+                    menu = Menu2();
+                    if (menu == "a")
+                    {
+                        storch.Beschreibung();
+                    }
+                    else if (menu == "b")
+                    {
+                        storch.Wegbeschreibung();
+                    }
+                    break;
+                case 5:
+                    vogel gans = new("Verwüster", "Gansus enormus", 2, true, false, "blutrot", true, "in der Mensa", "M-3");
+
+                    menu = Menu2();
+                    if (menu == "a")
+                    {
+                        gans.Beschreibung();
+                    }
+                    else if (menu == "b")
+                    {
+                        gans.Wegbeschreibung();
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Sie haben sich verklickt, wieder.");
+                    break;
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Tier.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooZwei
+{
+    internal class Tier
+    {
+        protected int beine;
+        protected string name;
+        protected string art;
+        protected bool flug;
+        protected bool fell;
+        protected bool carnivoire;
+        protected string markierung; //??
+        protected string ort;
+
+        public virtual void Beschreibung()
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"Dieses Tier heißt {name}.");
+            Console.WriteLine($"{name} gehört der Art {art} an.");
+            if (flug == true)
+                Console.WriteLine($"{name} ist flugfähig.");
+            if (carnivoire == true)
+                Console.WriteLine($"Die Art {art} sind Raubtiere. Sie jagen gerne Touristen.");
+            
+        }
+
+        public virtual void Wegbeschreibung()
+        {
+            Console.WriteLine($"Der Lebensort von {name} befindet sich {ort}.");
+            Console.WriteLine($"Die Markierung finden sie auf der Karte bei {markierung}.");
+        }
+    }
+
+    class mammal : Tier
+    {
+        private string fellfarbe;
+
+        public mammal(string name,string art, int beine, bool flug, bool fell, string fellfarbe, bool carn, string ort, string mark)
+        {
+            this.name = name;
+            this.beine = beine;
+            this.flug = flug;
+            this.fell= fell;
+            this.fellfarbe = fellfarbe;
+            this.carnivoire = carn;
+            this.art = art;
+            this.ort= ort;
+            this.markierung = mark;
+        }
+        public override void Beschreibung()
+        {
+            base.Beschreibung();
+            Console.WriteLine($"Mit {beine} Beinen kann {name} sich sehr schnell vorbewegen.");
+        }
+        public override void Wegbeschreibung()
+        {
+            base.Wegbeschreibung();
+        }
+    }
+
+    class reptile : Tier
+    {
+        private string scales;
+        public reptile(string name,string art, int beine, bool flug, bool fell, string scales, bool carn, string ort, string mark)
+        {
+            this.name = name;
+            this.beine = beine;
+            this.flug = flug;
+            this.fell = fell;
+            this.scales = scales;
+            this.carnivoire = carn;
+            this.art = art;
+            this.ort = ort;
+            this.markierung = mark;
+
+        }
+        public override void Beschreibung()
+        {
+            base.Beschreibung();
+            Console.WriteLine($"{name} ist ein Reptil.(Oder ein Nacktmull, die Wissenschaft ist sich nicht sicher.");
+            Console.WriteLine($"{name} hat die Schuppen in der Farbe {scales}");
+        }
+        public override void Wegbeschreibung()
+        {
+            base.Wegbeschreibung();
+        }
+    }
+
+    class vogel :Tier
+    {
+        private string gefieder;
+
+        public vogel(string name,string art, int beine, bool flug, bool fell, string gefieder, bool carn, string ort, string mark)
+        {
+            this.name = name;
+            this.beine = beine;
+            this.flug = flug;
+            this.fell = fell;
+            this.carnivoire = carn;
+            this.gefieder = gefieder;
+            this.art = art;
+            this.ort = ort;
+            this.markierung = mark;
+
+        }
+        public override void Beschreibung()
+        {
+            base.Beschreibung();
+            Console.WriteLine($"Das Gefieder der {art} hat überwiegend eine {gefieder}e Farbe.");
+        }
+        public override void Wegbeschreibung()
+        {
+            base.Wegbeschreibung();
+            
+        }
+
+    }
+}
